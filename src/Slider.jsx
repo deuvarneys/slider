fe17937f1215857d84355afc06343cea24ab5445
deuvarneys <deuvarneys@users.noreply.github.com>
2018-12-07 16:39:32 -0500

    Update Slider.jsx

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import Track from './common/Track';
 import createSlider from './common/createSlider';
 import * as utils from './utils';
@@ -7,167 +8,167 @@ import * as utils from './utils';
 class Slider extends React.Component {
   static displayName = 'Slider';
   static propTypes = {
-    defaultValue: PropTypes.number,
-    value: PropTypes.number,
-    disabled: PropTypes.bool,
-    ariaLabel: PropTypes.string,
-    ariaValueNow: PropTypes.number,
-    ariaValueText: PropTypes.string,
-    ariaValueMin: PropTypes.number,
-    ariaValueMax: PropTypes.number,
+      defaultValue: PropTypes.number,
+      value: PropTypes.number,
+      disabled: PropTypes.bool,
+      ariaLabel: PropTypes.string,
+      ariaValueNow: PropTypes.number,
+      ariaValueText: PropTypes.string,
+      ariaValueMin: PropTypes.number,
+      ariaValueMax: PropTypes.number,
   };
 
   static defaultProps = {};
 
   constructor(props) {
-    super(props);
+      super(props);
 
-    const defaultValue = props.defaultValue !== undefined ?
-            props.defaultValue : props.min;
-    const value = props.value !== undefined ?
-            props.value : defaultValue;
+      const defaultValue = props.defaultValue !== undefined ?
+          props.defaultValue : props.min;
+      const value = props.value !== undefined ?
+          props.value : defaultValue;
 
-    this.state = {
-      value: this.trimAlignValue(value),
-      dragging: false,
-    };
+      this.state = {
+          value: this.trimAlignValue(value),
+          dragging: false,
+      };
   }
 
   componentWillReceiveProps(nextProps) {
-    if (!('value' in nextProps || 'min' in nextProps || 'max' in nextProps)) return;
+      if (!('value' in nextProps || 'min' in nextProps || 'max' in nextProps)) {return;}
 
-    const prevValue = this.state.value;
-    const value = nextProps.value !== undefined ?
-            nextProps.value : prevValue;
-    const nextValue = this.trimAlignValue(value, nextProps);
-    if (nextValue === prevValue) return;
+      const prevValue = this.state.value;
+      const value = nextProps.value !== undefined ?
+          nextProps.value : prevValue;
+      const nextValue = this.trimAlignValue(value, nextProps);
+      if (nextValue === prevValue) {return;}
 
-    this.setState({ value: nextValue });
-    if (utils.isValueOutOfRange(value, nextProps)) {
-      this.props.onChange(nextValue);
-    }
+      this.setState({ value: nextValue });
+      if (utils.isValueOutOfRange(value, nextProps)) {
+          this.props.onChange(nextValue);
+      }
   }
 
   onChange(state) {
-    const props = this.props;
-    const isNotControlled = !('value' in props);
-    if (isNotControlled) {
-      this.setState(state);
-    }
+      const props = this.props;
+      const isNotControlled = !('value' in props);
+      if (isNotControlled) {
+          this.setState(state);
+      }
 
-    const changedValue = state.value;
-    props.onChange(changedValue);
+      const changedValue = state.value;
+      props.onChange(changedValue);
   }
 
   onStart(position) {
-    this.setState({ dragging: true });
-    const props = this.props;
-    const prevValue = this.getValue();
-    props.onBeforeChange(prevValue);
+      this.setState({ dragging: true });
+      const props = this.props;
+      const prevValue = this.getValue();
+      props.onBeforeChange(prevValue);
 
-    const value = this.calcValueByPos(position);
-    this.startValue = value;
-    this.startPosition = position;
+      const value = this.calcValueByPos(position);
+      this.startValue = value;
+      this.startPosition = position;
 
-    if (value === prevValue) return;
+      if (value === prevValue) {return;}
 
-    this.onChange({ value });
+      this.onChange({ value });
   }
 
   onEnd = () => {
-    this.setState({ dragging: false });
-    this.removeDocumentEvents();
-    this.props.onAfterChange(this.getValue());
+      this.setState({ dragging: false });
+      this.removeDocumentEvents();
+      this.props.onAfterChange(this.getValue());
   }
 
   onMove(e, position) {
-    utils.pauseEvent(e);
-    const state = this.state;
-    const value = this.calcValueByPos(position);
-    const oldValue = state.value;
-    if (value === oldValue) return;
+      utils.pauseEvent(e);
+      const state = this.state;
+      const value = this.calcValueByPos(position);
+      const oldValue = state.value;
+      if (value === oldValue) {return;}
 
-    this.onChange({ value });
+      this.onChange({ value });
   }
 
   onMoveByKeyboard(position, step) {
-    const value = this.calcValueByPos(position) + step;
+      const value = this.calcValueByPos(position) + step;
 
-    this.onChange({ value });
+      this.onChange({ value });
   }
 
   getValue() {
-    return this.state.value;
+      return this.state.value;
   }
 
   getLowerBound() {
-    return this.props.min;
+      return this.props.min;
   }
 
   getUpperBound() {
-    return this.state.value;
+      return this.state.value;
   }
 
   moveToMax() {
-    const { max } = this.props;
-    this.onChange({ value: max });
+      const { max } = this.props;
+      this.onChange({ value: max });
   }
 
   moveToMin() {
-    const { min } = this.props;
-    this.onChange({ value: min });
+      const { min } = this.props;
+      this.onChange({ value: min });
   }
 
   trimAlignValue(v, nextProps = {}) {
-    const mergedProps = { ...this.props, ...nextProps };
-    const val = utils.ensureValueInRange(v, mergedProps);
-    return utils.ensureValuePrecision(val, mergedProps);
+      const mergedProps = { ...this.props, ...nextProps };
+      const val = utils.ensureValueInRange(v, mergedProps);
+      return utils.ensureValuePrecision(val, mergedProps);
   }
 
   render() {
-    const {
-      prefixCls,
-      vertical,
-      included,
-      disabled,
-      minimumTrackTintColor,
-      ariaLabel,
-      ariaValueNow,
-      ariaValueText,
-      ariaValueMin,
-      ariaValueMax,
-      handle: handleGenerator,
-    } = this.props;
-    const { value, dragging } = this.state;
-    const offset = this.calcOffset(value);
-    const handle = handleGenerator({
-      className: `${prefixCls}-handle`,
-      vertical,
-      offset,
-      value,
-      dragging,
-      disabled,
-      ariaLabel,
-      ariaValueNow: ariaValueNow || value,
-      ariaValueText,
-      ariaValueMin,
-      ariaValueMax,
-      minimumTrackTintColor,
-      ref: h => this.saveHandle(0, h),
-    });
-    const track = (
-      <Track
-        className={`${prefixCls}-track`}
-        vertical={vertical}
-        included={included}
-        offset={0}
-        disabled={disabled}
-        length={offset}
-        minimumTrackTintColor={minimumTrackTintColor}
-      />
-    );
+      const {
+          prefixCls,
+          vertical,
+          included,
+          disabled,
+          minimumTrackTintColor,
+          ariaLabel,
+          ariaValueNow,
+          ariaValueText,
+          ariaValueMin,
+          ariaValueMax,
+          handle: handleGenerator,
+      } = this.props;
+      const { value, dragging } = this.state;
+      const offset = this.calcOffset(value);
+      const handle = handleGenerator({
+          className: `${prefixCls}-handle`,
+          vertical,
+          offset,
+          value,
+          dragging,
+          disabled,
+          ariaLabel,
+          ariaValueNow: ariaValueNow || value,
+          ariaValueText,
+          ariaValueMin,
+          ariaValueMax,
+          minimumTrackTintColor,
+          ref: h => this.saveHandle(0, h),
+      });
+      const track = (
+          <Track
+              className={`${prefixCls}-track`}
+              vertical={vertical}
+              included={included}
+              offset={0}
+              disabled={disabled}
+              length={offset}
+              minimumTrackTintColor={minimumTrackTintColor}
+          />
+      );
 
-    return { tracks: track, handles: handle };
+      return { tracks: track, handles: handle };
   }
 }
 
