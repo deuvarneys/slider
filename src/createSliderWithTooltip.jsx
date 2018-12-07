32fc9c3c207861ee9f4083495a56ca0e234636d2
deuvarneys <deuvarneys@users.noreply.github.com>
2018-12-07 16:39:55 -0500

    Update createSliderWithTooltip.jsx

--- src/createSliderWithTooltip.jsx
+++ src/createSliderWithTooltip.jsx
@@ -2,46 +2,48 @@ import React from 'react';
 import Tooltip from 'rc-tooltip';
 import Handle from './Handle';
 
+import PropTypes from 'prop-types';
+
 export default function createSliderWithTooltip(Component) {
-  return class ComponentWrapper extends React.Component {
+    return class ComponentWrapper extends React.Component {
     static propTypes = {
-      tipFormatter: React.PropTypes.func,
+        tipFormatter: PropTypes.func,
     };
     static defaultProps = {
-      tipFormatter(value) { return value; },
+        tipFormatter(value) { return value; },
     };
     constructor(props) {
-      super(props);
-      this.state = { visibles: {} };
+        super(props);
+        this.state = { visibles: {} };
     }
     handleTooltipVisibleChange = (index, visible) => {
-      this.setState({
-        visibles: {
-          ...this.state.visibles,
-          [index]: visible,
-        },
-      });
+        this.setState({
+            visibles: {
+                ...this.state.visibles,
+                [index]: visible,
+            },
+        });
     }
     handleWithTooltip = ({ value, dragging, index, disabled, ...restProps }) => {
-      const { tipFormatter } = this.props;
-      return (
-        <Tooltip
-          prefixCls="rc-slider-tooltip"
-          overlay={tipFormatter(value)}
-          visible={!disabled && (this.state.visibles[index] || dragging)}
-          placement="top"
-          key={index}
-        >
-          <Handle
-            {...restProps}
-            onMouseEnter={() => this.handleTooltipVisibleChange(index, true)}
-            onMouseLeave={() => this.handleTooltipVisibleChange(index, false)}
-          />
-        </Tooltip>
-      );
+        const { tipFormatter } = this.props;
+        return (
+            <Tooltip
+                prefixCls="rc-slider-tooltip"
+                overlay={tipFormatter(value)}
+                visible={!disabled && (this.state.visibles[index] || dragging)}
+                placement="top"
+                key={index}
+            >
+                <Handle
+                    {...restProps}
+                    onMouseEnter={() => this.handleTooltipVisibleChange(index, true)}
+                    onMouseLeave={() => this.handleTooltipVisibleChange(index, false)}
+                />
+            </Tooltip>
+        );
     }
     render() {
-      return <Component {...this.props} handle={this.handleWithTooltip} />;
+        return <Component {...this.props} handle={this.handleWithTooltip} />;
     }
-  };
+    };
 }
