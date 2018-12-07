98f7a040dbbcec003867dbf40288333becbd7614
deuvarneys <deuvarneys@users.noreply.github.com>
2018-12-07 16:38:49 -0500

    Update Handle.jsx

--- src/Handle.jsx
+++ src/Handle.jsx
@@ -1,51 +1,53 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+
+import React, { Component } from 'react';
 
 export default class Handle extends React.Component {
-  render() {
-    const {
-      className,
-      vertical,
-      offset,
-      minimumTrackTintColor,
-      disabled,
-      ariaLabel,
-      ariaValueText,
-      ariaValueNow,
-      ariaValueMin,
-      ariaValueMax,
-      ...restProps,
-    } = this.props;
-    const style = vertical ? { bottom: `${offset}%` } : { left: `${offset}%` };
-    const ariaOrientation = vertical ? 'vertical' : 'horizontal';
-    if (minimumTrackTintColor && !disabled) {
-      style.borderColor = minimumTrackTintColor;
+    render() {
+        const {
+            className,
+            vertical,
+            offset,
+            minimumTrackTintColor,
+            disabled,
+            ariaLabel,
+            ariaValueText,
+            ariaValueNow,
+            ariaValueMin,
+            ariaValueMax,
+            ...restProps,
+        } = this.props;
+        const style = vertical ? { bottom: `${offset}%` } : { left: `${offset}%` };
+        const ariaOrientation = vertical ? 'vertical' : 'horizontal';
+        if (minimumTrackTintColor && !disabled) {
+            style.borderColor = minimumTrackTintColor;
+        }
+        return (
+            <div
+                {...restProps}
+                className={className}
+                tabIndex={0}
+                role={'slider'}
+                style={style}
+                aria-label={ariaLabel}
+                aria-valuenow={ariaValueNow}
+                aria-orientation={ariaOrientation}
+                aria-valuetext={ariaValueText}
+                aria-valuemin={ariaValueMin}
+                aria-valuemax={ariaValueMax}
+            />);
     }
-    return (
-      <div
-        {...restProps}
-        className={className}
-        tabIndex={0}
-        role={'slider'}
-        style={style}
-        aria-label={ariaLabel}
-        aria-valuenow={ariaValueNow}
-        aria-orientation={ariaOrientation}
-        aria-valuetext={ariaValueText}
-        aria-valuemin={ariaValueMin}
-        aria-valuemax={ariaValueMax}
-      />);
-  }
 }
 
 Handle.propTypes = {
-  className: PropTypes.string,
-  vertical: PropTypes.bool,
-  offset: PropTypes.number,
-  minimumTrackTintColor: PropTypes.string,
-  disabled: PropTypes.bool,
-  ariaLabel: PropTypes.string,
-  ariaValueText: PropTypes.string,
-  ariaValueNow: PropTypes.number,
-  ariaValueMin: PropTypes.number,
-  ariaValueMax: PropTypes.number,
+    className: PropTypes.string,
+    vertical: PropTypes.bool,
+    offset: PropTypes.number,
+    minimumTrackTintColor: PropTypes.string,
+    disabled: PropTypes.bool,
+    ariaLabel: PropTypes.string,
+    ariaValueText: PropTypes.string,
+    ariaValueNow: PropTypes.number,
+    ariaValueMin: PropTypes.number,
+    ariaValueMax: PropTypes.number,
 };
